195eb63e8bce070aeb7ece291e4d695142fdb5b4
hieunba <hi3unb@gmail.com>
2016-11-15 22:09:31 +0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'opsworks_cookbook_demo'
+maintainer 'Nghiem Ba Hieu'
+maintainer_email 'hieu.nghiem.ba@bioelite.ro'
+license 'gplv3'
+description 'Installs/Configures opsworks_cookbook_demo'
+long_description 'Installs/Configures opsworks_cookbook_demo'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/opsworks_cookbook_demo/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/opsworks_cookbook_demo' if respond_to?(:source_url)
