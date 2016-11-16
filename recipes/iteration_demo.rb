94e7c1f7d78d0bb2ed254072ba9ee4b81f7e1b21
hieunba <hi3unb@gmail.com>
2016-11-16 20:30:25 +0700

    Add interation_demo recipe.

---
+++ recipes/iteration_demo.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: opsworks_cookbook_demo
+# Recipe:: iteration_demo
+#
+# Copyright (c) 2016 Nghiem Ba Hieu, All Rights Reserved.
+stack = search("aws_opsworks_stack").first
+Chef::Log.info("********* Content of 'custom_cookbook_source' ********")
+
+stack["custom_cookbooks_source"].each do |content|
+	Chef::Log.info("************** '#{content}' ***********")
+end
