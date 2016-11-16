24b63e2bd8f2bcfec6a8d0948b6aa5aba0db8085
hieunba <hi3unb@gmail.com>
2016-11-16 20:59:27 +0700

    Add demo dependencies_demo recipe.

---
+++ recipes/dependencies_demo.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: opsworks_cookbook_demo
+# Recipe:: dependencies_demo
+#
+# Copyright (c) 2016 Nghiem Ba Hieu, All Rights Reserved.
+application "Install NetHack" do
+	package "nethack.x86_64"
+end
