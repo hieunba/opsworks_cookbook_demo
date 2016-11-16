342be822837e56ce2d1db05ba0d077046ae6e0ac
Nghiem Ba Hieu <hi3unb@gmail.com>
2016-11-16 10:52:35 +0700

    Add recipe create a directory.

---
+++ recipes/create_directory.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: opsworks_cookbook_demo
+# Recipe:: create_directory
+#
+# Copyright (c) 2016 Nghiem Ba Hieu, All Rights Reserved.
+directory "Create a directory" do
+	group "root"
+	mode "0755"
+	ower "ubuntu"
+	path "/tmp/create-directory-demo"
+end
